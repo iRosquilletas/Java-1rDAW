2dbfa53961745f6038937beda1bb853358c5a913
samuel <samyelrandom@gmail.com>
2024-01-23 18:14:47 +0100

    jajA

--- unidad-7-dinamica/aceptaelreto/a238.java
+++ unidad-7-dinamica/aceptaelreto/a238.java
@@ -1,5 +1,4 @@
 package aceptaelreto;
-
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Scanner;
@@ -7,38 +6,43 @@ import java.util.Scanner;
 public class a238 {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
+        int dinero, sicarios;
+        
+
 
         while (true) {
-            int numBilletes = scanner.nextInt();
-            int numParticipantes = scanner.nextInt();
+            dinero = sc.nextInt();
+            sicarios = sc.nextInt();
 
-            if (numBilletes == 0 && numParticipantes == 0) {
+            if (dinero == 0 && sicarios == 0) {
                 break;
             }
 
             ArrayList<Integer> billetes = new ArrayList<>();
-            for (int i = 0; i < numBilletes; i++) {
-                billetes.add(scanner.nextInt());
+            for (int i = 0; i < dinero; i++) {
+                billetes.add(sc.nextInt());
             }
 
             Collections.sort(billetes, Collections.reverseOrder());
             ArrayList<Integer> reparto = new ArrayList<>();
-            int index = 0;
+            int cuenta = 0;
 
-            for (int i = 0; i < numParticipantes; i++) {
+            for (int i = 0; i < sicarios; i++) {
                 reparto.add(0);
+                
             }
 
-            for (int i = 0; i < numBilletes; i++) {
-                reparto.set(index, reparto.get(index) + billetes.get(i));
-                index = (index + 1) % numParticipantes;
+            for (int i = 0; i < dinero; i++) {
+                reparto.set(cuenta, reparto.get(cuenta) + billetes.get(i));
+                cuenta = (cuenta + 1) % sicarios;
+                
             }
 
-            for (int i = 0; i < numParticipantes; i++) {
+            for (int i = 0; i < sicarios; i++) {
                 System.out.print(reparto.get(i) + ":");
-                for (int j = 0; j < numBilletes; j++) {
-                    if (j < numBilletes - 1) {
+                for (int j = 0; j < dinero; j++) {
+                    if (j < dinero - 1) {
                         System.out.print(" " + billetes.get(j));
                     } else {
                         System.out.println(" " + billetes.get(j));
@@ -49,6 +53,6 @@ public class a238 {
             System.out.println("---");
         }
 
-        scanner.close();
+        sc.close();
     }
 }
